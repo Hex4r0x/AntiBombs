9373ec1e7de0e7a4e7126ece8a35be9600caec48
Hex4r0x <h.230756+github@i-lion.org>
2025-11-19 22:20:54 +0900

    init commit

---
+++ src/main/kotlin/com/hex4r0x/github/antiBombs/AntiBombs.kt
@@ -0,0 +1,14 @@
+package com.hex4r0x.github.antiBombs
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AntiBombs : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
